e8e5e1e70bb2c6369fd3a7f9063bd039895991bd
MarekWoj <149792485+MarekWoj@users.noreply.github.com>
2026-06-02 22:19:08 +0200

    Update selenium.addUser.test.js

--- frontend/tests/selenium.addUser.test.js
+++ frontend/tests/selenium.addUser.test.js
@@ -17,22 +17,20 @@ describe("Selenium Add User Tests", () => {
     test("should successfully add a new user", async() => {
         await driver.get("http://127.0.0.1:5500/frontend/index.html");
 
-        // 1. Przejście do zakładki Add User
+       
         await driver.findElement(By.linkText("Add User")).click();
 
-        // Oczekiwanie na pole id="name"
+   
         await driver.wait(until.elementLocated(By.id("name")), 5000);
 
-        // 2. Wypełnianie pól formularza
+     
         await driver.findElement(By.id("name")).sendKeys("Jan");
         await driver.findElement(By.id("surname")).sendKeys("Kowalski");
         await driver.findElement(By.id("email")).sendKeys("jan.kowalski@example.com");
         await driver.findElement(By.id("password")).sendKeys("BezpieczneHaslo123");
 
-        // 3. Wysyłanie formularza
         await driver.findElement(By.css("#userForm button[type='submit']")).click();
 
-        // === POPRAWKA: Kliknięcie "OK" na alercie "Dodano użytkownika" ===
         try {
             await driver.wait(until.alertIsPresent(), 3000);
             let alert = await driver.switchTo().alert();
@@ -41,10 +39,10 @@ describe("Selenium Add User Tests", () => {
             console.log("Brak alertu potwierdzającego dodanie użytkownika.");
         }
 
-        // 4. Przełączamy widok na zakładkę Users, aby tabela była widoczna
+
         await driver.findElement(By.linkText("Users")).click();
 
-        // 5. Bezpieczne sprawdzenie czy użytkownik trafił do tabeli
+
         try {
             await driver.wait(until.elementLocated(By.css("tbody tr")), 5000);
         } catch (err) {
