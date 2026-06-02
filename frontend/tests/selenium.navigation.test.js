89a306b1e3ebaf0a04c290e3212f75744763309e
MarekWoj <149792485+MarekWoj@users.noreply.github.com>
2026-06-02 22:21:09 +0200

    Update selenium.navigation.test.js

--- frontend/tests/selenium.navigation.test.js
+++ frontend/tests/selenium.navigation.test.js
@@ -17,10 +17,10 @@ describe("Selenium Navigation Tests", () => {
     test("should correctly navigate between views", async() => {
         await driver.get("http://127.0.0.1:5500/frontend/index.html");
 
-        // // Kliknięcie w link Users
+
         await driver.findElement(By.linkText("Users")).click();
 
-        // Dynamiczne oczekiwanie, aż element #users otrzyma klasę "active"
+
         await driver.wait(async() => {
             let cls = await driver.findElement(By.id("users")).getAttribute("class");
             return cls.includes("active");
@@ -29,11 +29,9 @@ describe("Selenium Navigation Tests", () => {
         let usersPage = await driver.findElement(By.id("users")).getAttribute("class");
         expect(usersPage.includes("active")).toBe(true);
 
-
-        // // Kliknięcie w link Add User
         await driver.findElement(By.linkText("Add User")).click();
 
-        // Dynamiczne oczekiwanie, aż element #add-user otrzyma klasę "active"
+
         await driver.wait(async() => {
             let cls = await driver.findElement(By.id("add-user")).getAttribute("class");
             return cls.includes("active");
@@ -43,10 +41,10 @@ describe("Selenium Navigation Tests", () => {
         expect(addPage.includes("active")).toBe(true);
 
 
-        // // Kliknięcie w link Dashboard
+
         await driver.findElement(By.linkText("Dashboard")).click();
 
-        // Dynamiczne oczekiwanie, aż element #dashboard otrzyma klasę "active"
+
         await driver.wait(async() => {
             let cls = await driver.findElement(By.id("dashboard")).getAttribute("class");
             return cls.includes("active");
